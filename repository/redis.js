5c2704b3636cb2d27749c12e242722024d9378ec
Alexander Cleoni <alexander.cleoni@gmail.com>
2024-10-10 15:19:31 -0400

    modify redis url

--- repository/redis.js
+++ repository/redis.js
@@ -1,8 +1,6 @@
 const Redis = require('ioredis');
 
 // Initialize the Redis client
-const redis = new Redis({
-  url: process.env.REDIS_URL
-});
+const redis = new Redis(process.env.REDIS_URL);
 
 module.exports = redis;
