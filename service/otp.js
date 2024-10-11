5e0974d407335d34f5462a07ed34a807c33a8b5e
Alexander Cleoni <alexander.cleoni@gmail.com>
2024-10-10 21:25:09 -0400

    ignore case for redisdb key

--- service/otp.js
+++ service/otp.js
@@ -9,7 +9,7 @@ async function generateOTP(key) {
   const expiry = 300;
 
   // Store OTP in Redis with an expiry time of 5 minutes
-  await redis.set(key, otp, 'EX', expiry);
+  await redis.set(key.toLowerCase(), otp, 'EX', expiry);
 
   console.log(`OTP for key ${key} is ${otp}`);
   return otp;
@@ -22,7 +22,7 @@ async function generateOTP(key) {
  * @returns {Promise<boolean>} - True if OTP is valid, false otherwise
  */
 async function verifyOTP(key, input) {
-  const otp = await redis.get(key);
+  const otp = await redis.get(key.toLowerCase());
   if (input === otp) {
     await redis.del(key);
     return true;
